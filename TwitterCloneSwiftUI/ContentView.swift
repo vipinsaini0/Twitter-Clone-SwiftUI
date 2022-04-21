2ee2a38d11e438fbef72cc9ac6afda01f5a3c248
Vipin Saini <vipinsaini0@users.noreply.github.com>
2022-04-21 11:05:55 +0530

    Initial Commit
    Project setup

---
+++ TwitterCloneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Vipin Saini on 21/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Vipin Saini on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
